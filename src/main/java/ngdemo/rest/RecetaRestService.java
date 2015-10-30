1fa0573e603b05cd8f0307ca829f4abfa7f18709
rodrijuarez <rodrigo.juarez.inf@gmail.com>
2015-10-30 19:40:02 -0300

    Se soluciona problema a través del cual no se retornaba correctamente la lista de objectos.

--- src/main/java/ngdemo/rest/RecetaRestService.java
+++ src/main/java/ngdemo/rest/RecetaRestService.java
@@ -1,7 +1,5 @@
 package ngdemo.rest;
 
-import java.util.List;
-
 import javax.transaction.NotSupportedException;
 import javax.transaction.SystemException;
 import javax.ws.rs.Consumes;
@@ -11,34 +9,33 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import ngdemo.domain.Receta;
-import ngdemo.service.RecetaService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import ngdemo.domain.Receta;
+import ngdemo.service.RecetaService;
 
 @Path("/recetas")
 @Component
 public class RecetaRestService {
 
-	@Autowired
-	private RecetaService recetaService;
+    @Autowired
+    private RecetaService recetaService;
 
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public List<Receta> getRecetas() {
-		return recetaService.getAll();
-	}
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Receta[] getRecetas() {
+        return recetaService.getAll().toArray(new Receta[] {});
+    }
 
-	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	public Receta create(final Receta receta) throws NotSupportedException,
-			SystemException, Exception {
-		return recetaService.create(receta);
-	}
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Receta create(final Receta receta) throws NotSupportedException, SystemException, Exception {
+        return recetaService.create(receta);
+    }
 
-	public void setRecetaService(final RecetaService recetaService) {
-		this.recetaService = recetaService;
-	}
+    public void setRecetaService(final RecetaService recetaService) {
+        this.recetaService = recetaService;
+    }
 }
--- src/main/webapp/js/services.js
+++ src/main/webapp/js/services.js
@@ -3,7 +3,11 @@ var services = angular.module('ngdemo.services', [ 'ngResource' ]);
 services.factory('RecetaFactory', function($resource) {
     return $resource('/ngdemo/rest/recetas', {}, {
         query : {
-            method : 'GET'
+            method : 'GET',
+            isArray : true
+        },
+        create : {
+            method : 'POST'
         }
     })
 });
